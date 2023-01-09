13f9b2291184b3e0dc5a13e9dca3746708cf2543
Benjamin Banwart <benjibanwart@gmail.com>
2023-01-09 17:38:25 -0600

    sending cookie data to backend on click

--- src/components/Applications.js
+++ src/components/Applications.js
@@ -8,22 +8,22 @@ import React from 'react'
 import { useEffect } from 'react'
 
 
-// const cookies = new Cookies();
+const cookies = new Cookies();
 
-// const handleButtonClick = () => {
-//     axios.get('http://localhost:3080/orderhive_refresh')
-//     .then((res)=> {
-//         console.log(res.data);
-//         cookies.set('access_key_id', res.data.access_key_id, { path: '/', expires: new Date(Date.now()+86400) })
-//         console.log(cookies.get('access_key_id'))
-//     });
-// }
-
+const handleButtonClick = () => {
+    axios.post('http://localhost:3080/orderhive_get_products', {
+        access_key_id: cookies.get('access_key_id_cookie').access_key_id,
+        secret_key_id: cookies.get('secret_key_cookie').secret_key,
+        session_token: cookies.get('session_token_cookie').session_token
+    })
+    .then((response) => {
+        console.log(response)
+    })
+}
 
 export default function Applications() {
 
     useEffect(() => {
-        const cookies = new Cookies();
             axios.get('http://localhost:3080/orderhive_refresh')
             .then((res)=> {
                 if(res.data === false){
@@ -57,7 +57,7 @@ export default function Applications() {
                 </Col>
                 <Col style={{ border: "1px black solid" }}>
                     <Button style={{ width: "8em", margin: "1em", marginLeft: "15em" }} 
-                    //onClick={handleButtonClick}
+                    onClick={handleButtonClick}
                     >Run</Button>
                 </Col>
             </Row>
