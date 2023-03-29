e077e97e30a66520db58895d9a9cb889f430010a
Benjamin Banwart <benjibanwart@gmail.com>
2023-03-29 08:09:20 -0500

    region to backend

--- src/components/Applications.js
+++ src/components/Applications.js
@@ -14,7 +14,8 @@ const handleButtonClick = () => {
     axios.post('http://localhost:4000/orderhive_get_products', {
         access_key_id: cookies.get('access_key_id_cookie').access_key_id,
         secret_key: cookies.get('secret_key_cookie').secret_key,
-        session_token: cookies.get('session_token_cookie').session_token
+        session_token: cookies.get('session_token_cookie').session_token,
+        region: cookies.get('region_cookie').region
     })
     .then((response) => {
         console.log(response)
